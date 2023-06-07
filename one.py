20dde9340aae682251238a094b23866092d9ad5d
Will Sutton <wsutton17@gmail.com>
2023-06-07 11:53:27 -0400

    starter

---
+++ one.py
@@ -0,0 +1,16 @@
+import time
+# import string
+# import random
+# import sqlite3
+# import os
+# import sys
+
+counter = 0
+while True:
+    counter += 1
+    print(counter)
+    time.sleep(1)
+
+    # with open('one.txt', 'w') as f:
+    #     f.write('Goodbye, World!')
+
---
+++ two.py
@@ -0,0 +1,12 @@
+import time
+# import string
+# import random
+# import sqlite3
+# import os
+# import sys
+
+counter = 1_000_000
+while True:
+    counter += 1
+    print(counter)
+    time.sleep(1)
