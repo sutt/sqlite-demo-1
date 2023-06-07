fb78d032be5e1bc03973138edbbf88f867937109
Will Sutton <wsutton17@gmail.com>
2023-06-07 18:45:53 -0400

    solution

--- one.py
+++ one.py
@@ -1,16 +1,44 @@
 import time
-# import string
-# import random
-# import sqlite3
-# import os
-# import sys
+import string
+import random
+import hashlib
+
+'''
+    The producer file, this will create and append to the queue.txt file
+    Adjust the settings below to change the difficulty of the hash
+'''
+
+COUNTER_PRINT_INTERVAL = 1_000_000
+DIFFICULTY = 5
+QUEUE_FN = 'queue.txt'
+
+
+def hash_msg(msg='coding everyday is fun'):
+    nonce_list = random.sample(string.ascii_lowercase, 10)
+    nonce = ''.join(nonce_list)
+    nonce + msg
+    b_nonce_msg = bytes(nonce + msg, 'utf-8')
+    output = hashlib.sha256(b_nonce_msg).hexdigest()
+    return output, nonce
+
 
 counter = 0
+
 while True:
+    
     counter += 1
-    print(counter)
-    time.sleep(1)
+    
+    output, nonce = hash_msg()
+    
+    if output.startswith('0' * DIFFICULTY):
+    
+        # append to file
+        # we write ~1 line/second, depending on cpu speed
+        with open(QUEUE_FN, 'a') as f:
+            f.write(f'{counter},{nonce},{output}\n')
 
-    with open('one.txt', 'w') as f:
-        f.write(str(counter))
+        print('FOUND ONE! ', counter, '\n', output[:10], nonce, '\n')
 
+    if counter % COUNTER_PRINT_INTERVAL == 0:
+        print('interval ', counter, '\n', output[:10], nonce, '\n')
+        
--- two.py
+++ two.py
@@ -1,19 +1,70 @@
 import time
-# import string
-# import random
-# import sqlite3
-# import os
-# import sys
+import sys
 
-# counter = 1_000_000
+'''
+    The consumer file, this will read from the queue.txt file in a loop
+    
+    This solution relies on looking at differences in the file each time it checks.
+    Would this scale over time?
+
+'''
+
+POLL_INTERVAL = 2
+POLL_COUNTER_PRINT_INTERVAL = 10
+QUEUE_FN = 'queue.txt'
+LOG_ERR = True
+LOG_WARN = True
+LOG_NOISY = False
+
+# startup, if we can't read the file, exit
+try:
+    with open(QUEUE_FN, 'r') as f:
+        lines = f.readlines()
+    num_events = len(lines)
+    print('Found', num_events, 'existing events in queue.txt')
+except Exception as e:
+    print('Error in startup(), exiting:', e)
+    sys.exit(1)
+
+
+# main loop
+counter = 0
 while True:
-    # counter += 1
-    # print(counter)
-    time.sleep(0.5)
-    with open('one.txt', 'r') as f:
-        try:
-            counter = int(f.read())
-        except Exception as e:
-            print(e)
-            counter = None
-    print(counter)
+    
+    counter += 1
+    
+    time.sleep(POLL_INTERVAL)
+    
+    try:
+        
+        with open(QUEUE_FN, 'r') as f:
+            lines = f.readlines()
+            _num_events = len(lines)
+        
+        if _num_events > num_events:
+            
+            num_new_events = _num_events - num_events
+            new_events = lines[-num_new_events:]
+            
+            # two.py has received new messages from one.py
+            # here we're just going to print it out
+            # but you could do something else, like:
+            # send a registration confirmation email
+            # tell a driver to go pick up a passenger
+            print('====FOUND', num_new_events, 'new events')
+            for event in new_events:
+                print(event)
+
+            num_events = _num_events
+        
+        elif _num_events < num_events:
+            if LOG_WARN: print('WARNING: queue.txt has fewer events than before')
+
+        else:
+            if LOG_NOISY: print('No new events')
+
+    except Exception as e:
+        if LOG_ERR: print('Error reading queue.txt:', e)
+
+    if counter % POLL_COUNTER_PRINT_INTERVAL == 0:
+        print('\nPolling interval', counter)
